dc2d9c635cc2a9f9ba3028a4d59aa9df846338a5
Diego Teixeira <diegosmteixeira@gmail.com>
2023-03-26 20:15:33 -0300

    implementing UserController

--- eCommerce.API/Controllers/UsersController.cs
+++ eCommerce.API/Controllers/UsersController.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eCommerce.API.Repositories;
+using eCommerce.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerce.API.Controllers
@@ -10,6 +12,59 @@ namespace eCommerce.API.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        
+        private readonly IUserRepository _repository;
+
+        public UsersController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var userList = _repository.Get();
+
+            return Ok(userList);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var user = _repository.Get(id);
+
+            if (user == null)
+                return NotFound("User not found");
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]User user)
+        {
+            _repository.Add(user);
+
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put([FromBody] User user)
+        {
+            var check = _repository.Get(user.Id);
+
+            if (check == null)
+                return NotFound("User not found");
+
+            _repository.Update(user);
+
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _repository.Delete(id);
+
+            return Ok();
+        }
     }
 }
