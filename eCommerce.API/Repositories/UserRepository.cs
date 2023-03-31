b03a9670328d538512dc52fcde50d4ae7c8300c6
Diego Teixeira <diegosmteixeira@gmail.com>
2023-03-31 13:31:06 -0300

    update UserRepository methods to use dbcontext instance

--- eCommerce.API/Repositories/UserRepository.cs
+++ eCommerce.API/Repositories/UserRepository.cs
@@ -2,37 +2,59 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using eCommerce.API.Database;
 using eCommerce.Models;
 
 namespace eCommerce.API.Repositories
 {
     public class UserRepository : IUserRepository
     {
-        public static List<User> _db = new List<User>();
+        private readonly eCommerceContext _db;
+        public UserRepository(eCommerceContext db)
+        {
+            _db = db;
+        }
+        
         public void Add(User user)
         {
-            _db.Add(user);
+            // Unit of Works (muliple operations at memory)
+            // Local Memory - EF Core
+            _db.Users.Add(user);
+
+            // Memory > DB (transaction) = 'use network connection'
+            _db.SaveChanges();
         }
 
         public void Delete(int id)
         {
-            _db.Remove(Get(id));
+            // Local Memory - EF Core
+            _db.Users.Remove(Get(id));
+
+            // Memory > DB (transaction) = 'use network connection'
+            _db.SaveChanges();
         }
 
         public List<User> Get()
         {
-            return _db;
+            // OrderBy() before ToList() => db operation *preferable
+            // OrderBy() after ToList() => .net c# operation
+            var list = _db.Users.ToList();
+            
+            return list;
         }
 
         public User Get(int id)
         {
-            return _db.Find(x => x.Id == id)!;
+            return _db.Users.Find(id)!;
         }
 
         public void Update(User user)
         {
-            _db.Remove(Get(user.Id));
-            _db.Add(user);
+            // Local Memory - EF Core
+            _db.Users.Update(user);
+
+            // Memory > DB
+            _db.SaveChanges();
         }
     }
 }
